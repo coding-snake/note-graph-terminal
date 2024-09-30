0f10283194b752a251f5c093845f7233c83d3c90
coding-snake <tabiszewski.damian.008@gmail.com>
2024-10-01 01:25:43 +0200

    better syntax

--- main.py
+++ main.py
@@ -1,34 +1,25 @@
 import json
 import os.path
 
-def create_default_json(file_path):
-    default_content = '''[
-    {
-        "id": 0,
-        "title": "root-node",
-        "type": "default",
-        "categ": [],
-        "req-prev": [],
-        "prev": [],
-        "next": [],
-        "opt": []
-    }
-]'''
+def create_data_file(file_path):
+    f_content = '''{
+    "nodes": [],
+    "categories": [],
+    "nodes_to_categories": [],
+    "edges": []
+}'''
     try:
         with open(file_path, 'w') as file:
-            file.write(default_content)
-        print(f"M: Json file created at '{file_path}'")
+            file.write(f_content)
+        print(f"M: Data file created at '{file_path}'")
     except Exception as e:
-        print(f"M: Failed to create json file due to {e}")
+        print(f"M: Failed to create data file due to \n{e}")
 
-
-
-def load_json(file_path):
-
+def read_data_file(file_path):
     if not os.path.exists(file_path):
         print(f"M: File at '{file_path}' not found")
-        print(f"Creating file...")
-        create_default_json(file_path)
+        print("M: Creating file...")
+        create_data_file(file_path)
     try:
         with open(file_path, 'r') as file:
             data = json.load(file)
@@ -37,165 +28,49 @@ def load_json(file_path):
         print(f"M: File at '{file_path}' not found")
         return None
 
-def save_json(file_path, data):
+def update_data_file(file_path, data, indent = 4):
     try:
         with open(file_path, 'w') as file:
-            json.dump(data, file, indent=4)
+            json.dump(data, file, indent=indent)
     except Exception as e:
-        print(f"M: Failed to save data to '{file_path}' due to {e}")
+        print(f"M: Failed to save data file at '{file_path}' due to \n{e}")
 
-def find_new_id(data):
-    f_id = 0
-    for obj in data:
-        f_id = int(max(int(obj["id"]), f_id))
-    f_id += 1
+def validate_and_fix_data_file(file_path):
+    def validate_and_fix_root_element(name_of_element):
+        if name_of_element not in data:
+            print(f"M: The element '" + name_of_element + f"' not found at '{file_path}'")
+            print(f"M: Adding " + name_of_element + "...")
+            data[name_of_element] = []
+            if name_of_element in data:
+                print(f"M: " + name_of_element + " added succesfuly")
+            else:
+                print(f"M: Failed to add " + name_of_element)
 
-    return f_id
+    data = read_data_file(file_path)
+    if not isinstance(data, dict):
+        print(f"M: The root element should be a dictionary")
+    else:
+        validate_and_fix_root_element("nodes")
+        validate_and_fix_root_element("categories")
+        validate_and_fix_root_element("nodes_to_categories")
+        validate_and_fix_root_element("edges")
 
-def query_attr(message_comp, message_or = "leave blank and press Enter to finish"):
-    list_obj = []
-    while True:
-        tmp = input("Add a " + message_comp + " (or " + message_or + "): ")
-        if tmp == "":
-            break
-        list_obj.append(tmp)
-    return list_obj
+        update_data_file(file_path, data)
 
+# -------- MAIN FUNCTION --------
 
-def new_node(file_path):
-    data = load_json(file_path)
+data_path = "data.json"
 
-    if data is None:
-        print("M: Data could not be loaded")
-        return
+print("Checking out the data file...")
+validate_and_fix_data_file(data_path)
 
-    new_id = find_new_id(data)
-
-    ans = ""
-    while ans == "":
-        ans = input("Enter the title: ")
-        if ans == "":
-            print("M: Title can not be empty!")
-
-    new_title = ans
-
-    new_type = input("Enter type of node (or press enter to chose 'base'): ")
-    if new_type == "":
-        new_type = "base"
-
-    new_categ = query_attr("category")
-    new_req_prev = query_attr("required previous node")
-    new_prev = query_attr("other previous node")
-    new_next = query_attr("next node")
-    new_opt = query_attr("optional node")
-
-    new_base_node = {
-        "id" : new_id,
-        "title" : new_title,
-        "type" : new_type,
-        "categ" : new_categ,
-        "req-prev" : new_req_prev,
-        "prev" : new_prev,
-        "next" : new_next,
-        "opt" : new_opt
-    }
-
-    data.append(new_base_node)
-    save_json(file_path, data)
-    print(f"M: New node added succesfully! \nid: {new_id} \ntitle: {new_title}")
-
-def add_rel(file_path):
-    data = load_json(file_path)
-
-    if data is None:
-        print("M: Data could not be loaded")
-        return
-
-    while True:
-        first_id = int(input("Enter id of first node: "))
-
-        first_node = None
-        for elem in data:
-            if elem["id"] == first_id:
-                first_node = elem
-                break
-
-        if first_node:
-            print(f"Note of an id {first_id}: {first_node["title"]}")
-            break
-        else:
-            print(f"Note with id {first_id} not found!")
-
-    options = [
-        "1) required previous",
-        "2) previous",
-        "3) next",
-        "4) optional"
-    ]
-
-    while True:
-        print("What relation would you like to add?")
-        for elem in options:
-            print(elem)
-        f_answer = int(input("---: "))
-
-        if 1 <= f_answer <= 4:
-            break
-
-    while True:
-        second_id = int(input("Enter id of second node: "))
-
-        second_node = None
-        for elem in data:
-            if elem["id"] == second_id:
-                second_node = elem
-                break
-
-        if first_node:
-            print(f"Note of an id {second_id}: {second_node["title"]}")
-            break
-        else:
-            print(f"Note with id {second_id} not found!")
-
-    if f_answer == 1:
-        print(f"The node {first_id}: {first_node["title"]}\nin a relation REQUIRED PREVIOUS with\nnode {second_id}: {second_node["title"]}")
-        first_node["req-prev"].append(second_id)
-        second_node["next"].append(first_id)
-    elif f_answer == 2:
-        print(
-            f"The node {first_id}: {first_node["title"]}\nin a relation PREVIOUS with\nnode {second_id}: {second_node["title"]}")
-        first_node["prev"].append(second_id)
-        second_node["next"].append(first_id)
-    elif f_answer == 3:
-        print(
-            f"The node {first_id}: {first_node["title"]}\nin a relation NEXT with\nnode {second_id}: {second_node["title"]}")
-        first_node["next"].append(second_id)
-        print("For the second node is this a relation REQUIRED PREVIOUS? (Y/n)")
-
-        tmp = ""
-        while tmp != "Y" and tmp != "n":
-            tmp = input()
-
-        if tmp == "Y":
-            second_node["req-prev"].append(first_id)
-        else:
-            second_node["prev"].append(first_id)
-    elif f_answer == 4:
-        print(
-            f"The node {first_id}: {first_node["title"]}\nin a relation OPTIONAL with\nnode {second_id}: {second_node["title"]}")
-        first_node["opt"].append(second_id)
-
-    save_json(file_path, data)
-    print(f"Nodes: \n{first_id} : {first_node["title"]} \n{second_id} : {second_node["title"]}\n UPDATED SUCCESFULLY")
-json_path = "data.json"
-
-print("Welcome to the note graph terminal!\nIf you are lost type 'help' to print out list of commands \nor type 'exit' to close the terminal")
+print("Welcome to the note terminal!")
+print("If you are lost type 'help' to get a list of commands")
+print("or type 'exit' to close the terminal")
 
 commands = [
-    "'exit' - exits the terminal",
     "'help' - prints out the list of commands",
-    "'add node' - adds another node",
-    "'add relation' - adds relation between two elements"
+    "'exit' - exits the terminal",
 ]
 
 answer = ""
@@ -203,17 +78,10 @@ while answer != "exit":
     answer = input("---: ")
 
     if answer == "exit":
-        continue
+        print("closing the terminal...")
     elif answer == "help":
         for c in commands:
             print(c)
-    elif answer == "add node":
-        new_node(json_path)
-    elif answer == "add relation":
-        add_rel(json_path)
     else:
-        print("If you are lost type 'help' to print out list of commands \nor type 'exit' to close the terminal")
-
-
-
-print('Hello')
+        print("If you are lost type 'help' to get a list of commands")
+        print("or type 'exit' to close the terminal")
